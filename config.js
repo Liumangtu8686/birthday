6ba3511c0be734fe37fc940fccb8c6823437e804
Liumangtu8686 <946148892@qq.com>
2021-03-07 15:00:30 +0800

    Update config.js

--- config.js
+++ config.js
@@ -28,13 +28,13 @@ var config = {
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "开始",
+        turn_on: "掌灯",
         play: "音乐",
-        bannar_coming: "颜色",
-        balloons_flying: "好像少点东西",
+        bannar_coming: "横条？",
+        balloons_flying: "好像还少点东西",
         cake_fadein: "蛋糕？",
         light_candle: "蜡烛？",
-        wish_message: "节日快乐",
+        wish_message: "happy birtuday and happy girls day!",
         story: "A MESSAGE FOR YOU",
     }
 };
